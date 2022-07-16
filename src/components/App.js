f26998fc65aa6ffa5c105e1bdda013d9ca09086e
Oscar Otee <45684861+Oscar-Otee@users.noreply.github.com>
2022-07-16 23:56:46 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,21 @@
 // create your App component here
+// create your App component here
+import React, { useState, useEffect } from "react";
+
+function App () {
+    const [dogImage, setDogImage] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+          .then((res) => res.json())
+          .then((data) =>{
+              setDogImage(data.message);
+              setIsLoaded(true);
+          });
+    }, []);
+    if (!isLoaded) return <p>Loading...</p>;
+    return <img src={dogImage} alt="A Random Dog" />;
+}
+
+export default App;
